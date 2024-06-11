044f1fb2672e5b1a89783f0b06ee3f814f937086
Manon74909 <mmcvincent04@gmail.com>
2024-06-11 08:38:01 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -123,7 +123,7 @@ const options = {
 }
 
 function setup(){
-  canvas = createCanvas(640,640); 
+  canvas = createCanvas(window Width,window Height); 
 
   // Create a tile map with the options declared
   myMap = mappa.tileMap(options); 
